430303db1e1308e8a4aa12b11eec2bac2531cfb5
Christian Knittl-Frank <lcnittl@gmail.com>
2020-09-12 16:36:05 +0200

    Update setup.py, LICENSE and AUTHORS

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ try:
     long_desc = f.read()
 finally:
     f.close()
-    
+
 requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-email',
     version='0.1',
-    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
+    url='https://github.com/sphinx-contrib/email',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-email',
-    license='BSD',
-    author='Kevin Teague',
-    author_email='kevin@bud.ca',
+    license = 'BSD-3',
+    license_files = 'LICENSE',
+    author='Christian Knittl-Frank',
+    author_email='lcnittl@gmail.com',
     description='Sphinx email obfuscation extension',
     long_description=long_desc,
     zip_safe=False,
