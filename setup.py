ec09ac2a47b0b8dca2bb84491b20a888b86cae6a
Christian Knittl-Frank <lcnittl@gmail.com>
2020-09-12 17:14:10 +0200

    pre-commit run --all

--- setup.py
+++ setup.py
@@ -1,41 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-f = open('README.rst', 'r')
-try:
-    long_desc = f.read()
-finally:
-    f.close()
+with open("README.rst", "r") as file:
+    long_desc = file.read()
 
-requires = ['Sphinx>=0.6']
+requires = ["Sphinx>=0.6"]
 
 setup(
-    name='sphinxcontrib-email',
-    version='0.1',
-    url='https://github.com/sphinx-contrib/email',
-    download_url='http://pypi.python.org/pypi/sphinxcontrib-email',
-    license = 'BSD-3',
-    license_files = 'LICENSE',
-    author='Christian Knittl-Frank',
-    author_email='lcnittl@gmail.com',
-    description='Sphinx email obfuscation extension',
+    name="sphinxcontrib-email",
+    version="0.1",
+    url="https://github.com/sphinx-contrib/email",
+    download_url="http://pypi.python.org/pypi/sphinxcontrib-email",
+    license = "BSD-3",
+    license_files = "LICENSE",
+    author="Christian Knittl-Frank",
+    author_email="lcnittl@gmail.com",
+    description="Sphinx email obfuscation extension",
     long_description=long_desc,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
     ],
-    platforms='any',
+    platforms="any",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requires,
-    namespace_packages=['sphinxcontrib'],
+    namespace_packages=["sphinxcontrib"],
 )
--- sphinxcontrib/email.py
+++ sphinxcontrib/email.py
@@ -1,4 +1,4 @@
-# E-mail obfuscation role for Sphinx.
+import re
 
 from docutils import nodes
 
@@ -9,30 +9,29 @@ from docutils import nodes
 # and was was released by Kevin Teague <kevin at bud ca> under
 # a BSD license.
 
-import re
 
 try:
-    maketrans = ''.maketrans
+    maketrans = "".maketrans
 except AttributeError:
     # fallback for Python 2
     from string import maketrans
 
 rot_13_trans = maketrans(
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-    'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
 )
 
-def rot_13_encrypt(line):
-    """Rotate 13 encryption.
 
-    """
+def rot_13_encrypt(line):
+    """Rotate 13 encryption."""
     line = line.translate(rot_13_trans)
-    line = re.sub('(?=[\\"])', r'\\', line)
-    line = re.sub('\n', r'\n', line)
-    line = re.sub('@', r'\\100', line)
-    line = re.sub('\.', r'\\056', line)
-    line = re.sub('/', r'\\057', line)
+    line = re.sub('(?=[\\"])', r"\\", line)
+    line = re.sub("\n", r"\n", line)
+    line = re.sub("@", r"\\100", line)
+    line = re.sub("\.", r"\\056", line)
+    line = re.sub("/", r"\\057", line)
     return line
+
 
 def js_obfuscated_text(text):
     """ROT 13 encryption with embedded in Javascript code to decrypt
@@ -44,45 +43,48 @@ def js_obfuscated_text(text):
               function(c){
                 return String.fromCharCode(
                 (c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
-                </script>""" % rot_13_encrypt(text)
+                </script>""" % rot_13_encrypt(
+        text
+    )
+
 
 def js_obfuscated_mailto(email, displayname=None):
-    """ROT 13 encryption within an Anchor tag w/ a mailto: attribute
-
-    """
+    """ROT 13 encryption within an Anchor tag w/ a mailto: attribute"""
     if not displayname:
         displayname = email
-    return js_obfuscated_text("""<a href="mailto:%s">%s</a>""" % (
-        email, displayname
-    ))
+    return js_obfuscated_text("""<a href="mailto:%s">%s</a>""" % (email, displayname))
+
 
 # -- end bud.nospam
 
-def email_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+
+def email_role(
+    typ, rawtext, text, lineno, inliner, options={}, content=[]  # noqa B006
+):
     """
     Role to obfuscate e-mail addresses.
     """
     try:
-        text = text.decode('utf-8').encode('utf-8')
+        text = text.decode("utf-8").encode("utf-8")
     except AttributeError:
         # fallback for Python 3
         pass
 
     # Handle addresses of the form "Name <name@domain.org>"
-    if '<' in text and '>' in text:
-        name, email = text.split('<')
-        email = email.split('>')[0]
-    elif '(' in text and ')' in text:
-        name, email = text.split('(')
-        email = email.split(')')[0]
+    if "<" in text and ">" in text:
+        name, email = text.split("<")
+        email = email.split(">")[0]
+    elif "(" in text and ")" in text:
+        name, email = text.split("(")
+        email = email.split(")")[0]
     else:
         name = text
         email = name
 
     obfuscated = js_obfuscated_mailto(email, displayname=name)
-    node = nodes.raw('', obfuscated, format='html')
+    node = nodes.raw("", obfuscated, format="html")
     return [node], []
 
-def setup(app):
-    app.add_role('email', email_role)
 
+def setup(app):
+    app.add_role("email", email_role)
