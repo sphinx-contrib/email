4e04ebd1e0f9d13e622c08111deca4b60c8f4d3e
Christian Knittl-Frank <lcnittl@gmail.com>
2020-09-12 17:49:24 +0200

    Cleanup 'email.py'

--- sphinxcontrib/email.py
+++ sphinxcontrib/email.py
@@ -1,12 +1,13 @@
 import re
+import textwrap
 
 from docutils import nodes
 
-# The obfuscation code was taken from
+# The obfuscation code was adapted from
 #
 #   http://pypi.python.org/pypi/bud.nospam
 #
-# and was was released by Kevin Teague <kevin at bud ca> under
+# where was released by Kevin Teague <kevin at bud ca> under
 # a BSD license.
 
 
@@ -19,34 +20,37 @@ rot_13_trans = str.maketrans(
 def rot_13_encrypt(line):
     """Rotate 13 encryption."""
     line = line.translate(rot_13_trans)
-    line = re.sub('(?=[\\"])', r"\\", line)
+    line = re.sub(r"(?=[\"])", r"\\", line)
     line = re.sub("\n", r"\n", line)
-    line = re.sub("@", r"\\100", line)
-    line = re.sub("\.", r"\\056", line)
-    line = re.sub("/", r"\\057", line)
+    line = re.sub(r"@", r"\\100", line)
+    line = re.sub(r"\.", r"\\056", line)
+    line = re.sub(r"/", r"\\057", line)
     return line
 
 
 def js_obfuscated_text(text):
-    """ROT 13 encryption with embedded in Javascript code to decrypt
-    in the browser.
-
-    """
-    return """<script type="text/javascript">document.write(
-              "%s".replace(/[a-zA-Z]/g,
-              function(c){
-                return String.fromCharCode(
-                (c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
-                </script>""" % rot_13_encrypt(
-        text
-    )
+    """ROT 13 encryption with embedded in Javascript code to decrypt in the browser."""
+    snippet = textwrap.dedent(
+        """\
+        <script type="text/javascript">document.write(
+            "{text}".replace(/[a-zA-Z]/g,
+                function(c){{
+                    return String.fromCharCode(
+                        (c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26
+                    );
+                }}
+            )
+        );</script>"""
+    ).format(text=rot_13_encrypt(text))
+    return snippet
 
 
 def js_obfuscated_mailto(email, displayname=None):
     """ROT 13 encryption within an Anchor tag w/ a mailto: attribute"""
-    if not displayname:
-        displayname = email
-    return js_obfuscated_text("""<a href="mailto:%s">%s</a>""" % (email, displayname))
+    snippet = """<a href="mailto:{email}">{displayname}</a>""".format(
+        email=email, displayname=(displayname or email)
+    )
+    return js_obfuscated_text(snippet)
 
 
 # -- end bud.nospam
