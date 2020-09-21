14f28a170d01f222785b4f34e2887ea453e438b4
Christian Knittl-Frank <lcnittl@gmail.com>
2020-09-21 04:06:39 +0200

    fix(version): Return extension version

--- src/sphinxcontrib/email/__init__.py
+++ src/sphinxcontrib/email/__init__.py
@@ -1,11 +1,18 @@
+from __future__ import annotations
+
+from typing import Any
+
 from sphinx.application import Sphinx
 
 from .handlers import html_page_context_handler
 from .roles import EmailRole
 
+__version__ = "0.3.1"
 
-def setup(app: Sphinx) -> None:
+
+def setup(app: Sphinx) -> dict[str, Any]:
     app.add_config_value(name="email_automode", default=False, rebuild="env")
     if app.config["email_automode"]:
         app.connect("html-page-context", html_page_context_handler)
     app.add_role("email", EmailRole())
+    return {"version": __version__}
