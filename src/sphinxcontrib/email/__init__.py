184bdde99740f3a2d80b9eeca63384cf7da3a43c
semantic-release <semantic-release>
2020-09-21 04:09:29 +0200

    Bump version to v0.3.2
    Automatically generated by python-semantic-release

--- src/sphinxcontrib/email/__init__.py
+++ src/sphinxcontrib/email/__init__.py
@@ -7,7 +7,7 @@ from sphinx.application import Sphinx
 from .handlers import html_page_context_handler
 from .roles import EmailRole
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 def setup(app: Sphinx) -> dict[str, Any]:
