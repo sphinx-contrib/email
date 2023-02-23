7f5b58d0ffe09901146f4395448d8ef8c034b36a
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-02-23 11:49:43 +0100

    modernize packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-import setuptools
+from setuptools import setup
 
-if __name__ == "__main__":
-    setuptools.setup(
-        use_scm_version=True,
-        setup_requires="setuptools-scm>=4.1",
-    )
+setup()
